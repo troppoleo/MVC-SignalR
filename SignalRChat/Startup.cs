46989e77b1e91a84a07feef6fc3ad182f3bb1fb7
troppoleo <troppoleo@gmail.com>
2015-12-05 11:53:36 +0100

    MVC SignalR

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace SignalRChat
+//{
+//    public class Startup
+//    {
+//    }
+//}
+
+
+using Owin;
+using Microsoft.Owin;
+
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
